79eb3fc81bcc29ebe951ed5cca78a86ea74b9e17
Venkata Sumanth Bathina <sumanthbv05>
2025-05-27 19:49:24 +0530

    Initial commit

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ import authRoutes from './routes/AuthRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ['hostel-world-frontend.vercel.app'],
+    credentials: true
+}))
 app.use(express.json());
 
 app.use('/api/hostels', hostelRoutes);
