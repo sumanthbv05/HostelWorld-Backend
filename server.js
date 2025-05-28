4669bf096cb74f25a1cc01da1b4d7937569cf6a9
Venkata Sumanth Bathina <sumanthbv05>
2025-05-28 09:24:23 +0530

    inintal

--- server.js
+++ server.js
@@ -8,13 +8,7 @@ import authRoutes from './routes/AuthRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors(
-    {
-        origin: [""],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/hostels', hostelRoutes);
